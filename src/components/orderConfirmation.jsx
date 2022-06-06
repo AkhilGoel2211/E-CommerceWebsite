c65b8392de16799684fc333c33f95ed080ea78be
Akhil Goel <akhil1goel@gmail.com>
2022-06-06 19:49:45 +0530

    Order Confirmation Page Completed.

--- src/components/orderConfirmation.jsx
+++ src/components/orderConfirmation.jsx
@@ -1,7 +1,20 @@
-import React, {Component} from 'react';
+import React, {Component} from "react";
 
 const OrderConfirmed = () => {
-  return (<h1>Order Success</h1>);
+  return (
+    <React.Fragment>
+      <center style={{color: "dodgerblue"}}>
+        <br />
+        <br />
+        <h1>Order Success</h1>
+        <h2>It shall arrive at your Doorstep Soon!!</h2>
+        <br />
+        <br />
+        <br />
+        <h2>Thank you for Shopping with Us :)</h2>
+      </center>
+    </React.Fragment>
+  );
 };
 
 export default OrderConfirmed;
